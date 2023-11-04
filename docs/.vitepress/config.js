5d42b0ff5de1292814bf23d3e4386e178b1e492f
Novrizal <novrizalcode@gmail.com>
2023-11-04 16:15:17 +0700

    add blog-gue

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+export default {
+    title: 'Novrizal',
+    description: 'Just playing around.',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Blog', link: '/blog' },
+        { text: 'Jurnal', link: '/jurnal' },
+
+        { text: 'External', link: 'https://google.com' },
+      ],
+    },
+  };

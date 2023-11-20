25d25d659e0744e67dbea6966e3a384bc193a5f4
novrizalmvg <novrizalmvg@gmail.com>
2023-11-20 17:18:12 +0700

    add api

--- docs/components/ArticleDetail.vue
+++ docs/components/ArticleDetail.vue
@@ -8,17 +8,24 @@
     </div>
     <div class="interactions">
       <div class="like-comment">
-        <button @click="likePost" class="like-button">
-          <i class="far fa-thumbs-up"></i>
-          <span>Like</span>
-        </button>
-        <button class="comment-button">
-          <i class="far fa-comment-alt"></i>
-          <span>Comment</span>
-        </button>
+        <!-- Button for liking -->
+        <div class="like-container">
+          <button @click="likePost" class="like-button">
+            <i class="far fa-thumbs-up"></i>
+            <span>Like {{ total_like }}</span>
+          </button>
+          <!-- Display total likes -->
+        </div>
+        <div class="like-container">
+          <button @click="likePost" class="like-button">
+            <i class="fa-solid fa-comment"></i>
+            <span>Comment {{ total_comment }}</span>
+          </button>
+          <!-- Display total likes -->
+        </div>
       </div>
 
-      <div class="comment-section">
+      <!-- <div class="comment-section">
         <div class="add-comment">
           <input
             v-model="comment"
@@ -40,7 +47,7 @@
             </li>
           </ul>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -53,13 +60,17 @@ export default {
   data() {
     return {
       post: {},
+      total_like: 0,
       articleId: null,
+      total_comment: 0,
     };
   },
   mounted() {
     const urlParams = new URLSearchParams(window.location.search);
     this.articleId = urlParams.get("id");
     this.fetchJurnal();
+    this.fetchLike();
+    this.fetchComment();
   },
   methods: {
     fetchJurnal() {
@@ -70,16 +81,15 @@ export default {
         });
     },
     likePost() {
+      this.fetchLike();
       const payload = {
         post_id: this.articleId,
-        email: "sopyan@gmail",
+        email: "sopyan@gmail.com",
         total: 1,
       };
-
       axios
         .post(`${server.baseURL}/like/`, payload, {
           headers: {
-            "Cache-Control": "no-cache",
             "Content-Type": "application/x-www-form-urlencoded",
           },
         })
@@ -92,11 +102,46 @@ export default {
           // Handle the error, display a message, or perform other actions
         });
     },
+    fetchLike() {
+      axios.get(`${server.baseURL}/like/`).then((data) => {
+        const totalLikes = data.data.reduce((total, currentItem) => {
+          // Konversi nilai 'total' dan 'currentItem.total' ke tipe number dan menjumlahkannya
+          return total + parseInt(currentItem.total);
+        }, 0);
+        console.log("masuk sini", totalLikes);
+        this.total_like = totalLikes;
+      });
+    },
+    fetchComment() {
+      axios.get(`${server.baseURL}/comment/`).then((data) => {
+        this.total_comment = data.data.length;
+      });
+    },
   },
 };
 </script>
 
 <style>
+/* Your existing styles */
+
+/* New styles for positioning total likes */
+.interactions {
+  /* ... */
+  display: flex;
+  justify-content: center;
+}
+
+.like-container {
+  display: flex;
+  align-items: center;
+}
+
+.total-likes {
+  margin-left: 8px;
+  font-size: 14px;
+  color: #606770;
+}
+
 .fa-thumbs-up:before {
   content: "\f164"; /* Unicode for Facebook thumbs-up icon */
   font-family: "Font Awesome 5 Free";
